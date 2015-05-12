cce8d86db96278fb58b5c0a55af988078fb3b606
Marlena Compton and Thomas Bukowski <me@neodude.net>
2015-05-11 20:17:02 -0700

    User can create a valid post
    Add tests for post controller.
    Add form for new post
    Add create and new to post controller

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -16,7 +16,30 @@ describe PostsController do
       expect(response).to be_success
       expect(response).to have_http_status(200)
     end
+
+    it "sets @post" do
+      get :new
+      expect(assigns[:post]).to be_a_kind_of(Post)
+    end
   end
 
+  describe '#create' do
+    let(:title) { 'title' }
+    let(:content) { 'content' }
 
+    it 'creates a post' do
+      expect do
+        post :create, post: {title: title, content: content}
+      end.to change(Post, :count).by(1)
+      last_post = Post.last
+      expect(last_post.title).to eq(title)
+      expect(last_post.content).to eq(content)
+    end
+
+    xit "responds successfully with an HTTP 200 status code" do
+      post :create, post: {title: title, content: content}
+      expect(response).to redirect_to(post_path(Post.last))
+      expect(response).to have_http_status(200)
+    end
+  end
 end
